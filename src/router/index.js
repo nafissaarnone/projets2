90617ae3da1c28190087dfefa4b81cd66a38b4b5
nafissaarnone <95024629+nafissaarnone@users.noreply.github.com>
2022-05-31 15:47:37 +0200

    mise a jour router

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,52 @@
+
 import { createRouter, createWebHistory } from 'vue-router'
-import AccueilView from '../views/AccueilView.vue'
+
+import ArtisteView from '../views/ArtisteView.vue'
+
 import artistesView from '../views/artistesView.vue'
 
+import programmationView from '../views/programmationView.vue'
+
+import contactView from '../views/contactView.vue'
+
+import mentionslegaleView from '../views/mentionslegaleView.vue'
+
+import festivalView from '../views/festivalView.vue'
+
+import concertView from '../views/concertView.vue'
+
+
+
+
 const router = createRouter({
+
   history: createWebHistory(import.meta.env.BASE_URL),
+
   routes: [
+
     { path: '/', name: 'Accueil', component: AccueilView },
-    { path: '/artistes', name: 'Artistes', component: artistesView },
+
+    { path: '/artistes', name: 'artistes', component: artistesView },
+
+    { path: '/artiste', name: 'Artiste', component: ArtisteView },
+
+    { path: '/concert', name: 'concert', component: concertView },
+
+    { path: '/contact', name: 'contact', component: contactView },
+
+    { path: '/festival', name: 'festival', component: festivalView },
+
+    { path: '/mentionslegale', name: 'Mentions Légale', component: mentionslegaleView },
+
+    { path: '/programme', name: 'Programme', component: ProgrammationView }
+
+
+
     // ici les autre routes
+
   ]
+
 })
+
 
 export default router
