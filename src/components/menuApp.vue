dccc6f8f201ce1290338b87c68c082dc84c2c8cc
nafissaarnone <95024629+nafissaarnone@users.noreply.github.com>
2022-05-31 15:33:33 +0200

    menu

--- src/components/menuApp.vue
+++ src/components/menuApp.vue
@@ -1,42 +1,56 @@
 <template>
-    
-<transition
-          enter-active-class="transition duration-3000 ease-out"
-          enter-from-class="transform scale-95 opacity-0"
-          enter-to-class="transform scale-3000 opacity-100"
-          leave-active-class="transition duration-75 ease-out"
-          leave-from-class="transform scale-100 opacity-100"
-          leave-to-class="transform scale-95 opacity-0"
-        >
-          <MenuItems class="flex flex-col bg-black">
-            <MenuItem v-slot="{ active }">
-              
-                <RouterLink to="/" :class="{ 'text-white': active }" class="text-blue-600 underline"> 
-                  <div class="flex my-3">
-                    <HomeIcon class="stroke-white w-6 mr-3" />
-                    <span class="font-roboto">Accueil</span>
-                  </div>
-                </RouterLink>
-              
-            </MenuItem>
-            
-            <MenuItem>
-              <RouterLink to="/artistes" :class="{ 'text-white': active }" class="text-blue-600 underline">
-                <div class="flex mb-3">
-                    <NewspaperIcon class="stroke-white w-6 mr-3 "/>
-                     <span class="font-roboto">Programme</span>
-                </div> 
-              </RouterLink>
-            </MenuItem>
+  <nav>
+      <div class="bg-transparent">
+        <div class="max-w-7xl mx-auto">
+            <div class="flex justify-between">
+                <!-- Menu -->
+                <div class="flex space-x-4 justify-between">
+                    <!-- Logo -->
+                    <div class="flex items-center justify-between py-5 px-2">
+                        <RouterLink to="/"><img src="../assets/lys.svg"></RouterLink>
+                    </div>
+                    <!-- Menu -->
+                    <div class="hidden text-white ipad_pro:flex items-center space-x-1">
+            <RouterLink to="/" class="py-5 px-2 hover:font-bold">Accueil</RouterLink>
+            <RouterLink to="/artistes" class="py-5 px-2 hover:font-bold">Artistes</RouterLink>
+            <RouterLink to="/contact" class="py-5 px-2 hover:font-bold">Contact</RouterLink>
+            <RouterLink to="/festival" class="py-5 px-2 hover:font-bold">Festival</RouterLink>
+            <RouterLink to="/mentionslégale" class="py-5 px-2 hover:font-bold">Mention Légale</RouterLink>
+            <RouterLink to="/programmation" class="py-5 px-2 hover:font-bold ">Programme</RouterLink>
+                    
+                    </div>
+                </div>
+               
 
-          </MenuItems>
-        </transition>`
-        </template>
+                <!-- Bouton menu -->
+                <div class="ipad_pro:hidden flex items-center">
+                    <button @click="menuVisible = !menuVisible" class=" py-5 px-2 "> 
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+                
+            </div>
+        </div>
+      </div>
+      <!-- Menu mobile -->
+      <div class="text-white bg-transparent ipad_pro:hidden" :class="{hidden: menuVisible}">
+      <RouterLink to="/" class="py-5 px-2 hover:font-bold">Accueil</RouterLink>
+            <RouterLink to="/artistes" class="block py-2 px-4  bg-transparent">Artistes</RouterLink>
+            <RouterLink to="/contact" class="block py-2 px-4  bg-transparent">Contact</RouterLink>
+            <RouterLink to="/festival" class="block py-2 px-4  bg-transparent">Festival</RouterLink>
+            <RouterLink to="/mentionlegale" class="block py-2 px-4  bg-transparent">Mention Légale</RouterLink>
+            <RouterLink to="/programme" class="block py-2 px-4  bg-transparent ">Programme</RouterLink>
+      </div>
+  </nav>
 
-        <script>
-import { MenuIcon, HomeIcon, NewspaperIcon, UserGroupIcon, MailOpenIcon } from "@heroicons/vue/outline";
-export default {
-  components: { MenuIcon, HomeIcon, NewspaperIcon, UserGroupIcon,  MailOpenIcon   },
-};
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const menuVisible = ref(true);
+
 
 </script>
