8c6b57f02a749012d4d13c7498aa3835fcfd43c4
nafissaarnone <95024629+nafissaarnone@users.noreply.github.com>
2022-06-01 15:52:11 +0200

    liste artistes

--- src/views/artistesView.vue
+++ src/views/artistesView.vue
@@ -1,8 +1,48 @@
 <template>
-  <div class="w-[200px] h-[74px]">
-  <div class="w-[2px] h-[74px] absolute left-[-1.5px] top-[46.5px] bg-[#e6f2dd]"></div>
-  <p class="absolute left-[118px] top-[61px] text-[33px] text-center font-bold text-left text-black">
+  
+  <p class=" mt-8 mb-4 w-full text-[33px] bg-[#e6f2dd] font-bold text-center text-black">
     ARTISTES
   </p>
-</div>
+  <div>
+    <div class="flex justify-end">
+      <div class="m-1 ">
+        <img class="" src="../../image/savara.jpg" alt="savara">
+        <p class="relative top-[-50px] left-[40px] z-10 w-[100px] bg-white text-center text-[18px] "> Savara</p>
+      </div>
+      <div class="m-1 ">
+        <img src="../../image/aya.jpg" alt="aya">
+        <p class="relative top-[-50px] left-[40px] z-10 w-[200px] bg-white text-center text-[18px] "> Aya nakamura</p>
+      </div>
+    </div>
+    <div class="flex justify-end">
+      <div class="m-1 ">
+        <img src="../../image/wizkid.jpg" alt="beyonce">
+        <p class="relative top-[-50px] left-[40px] z-10 w-[100px] bg-white text-center text-[18px] "> Wizkid</p>
+      </div>
+      <div class="m-1 ">
+        <img class="" src="../../image/dbngogo.jpg" alt="dbngogo">
+        <p class="relative top-[-50px] left-[40px] z-10 w-[100px] bg-white text-center text-[18px] "> dbngogo</p>
+      </div>
+      <div class="m-1 ">
+        <img src="../../image/beyonce.jpg" alt="beyonce">
+        <p class="relative top-[-50px] left-[10px] z-10 w-[90px] bg-white text-center text-[18px]"> Beyonce</p>
+      </div>
+    </div>
+    <div class="flex justify-end">
+      <div class="m-1 ">
+        <img src="../../image/shatta.jpg" alt="SHATTA WALL">
+        <p class="relative top-[-50px] left-[40px] z-10 w-[200px] bg-white text-center text-[18px] ">Shatta wall</p>
+      </div>
+      <div class="m-1 ">
+        <img class="" src="../../image/kyala.jpg" alt="KYALA">
+        <p class="relative top-[-50px] left-[40px] z-10 w-[100px] bg-white text-center text-[18px] ">Kyala</p>
+      </div>
+    </div>
+  </div>
+
+
 </template>
+
+<script>
+
+</script>
