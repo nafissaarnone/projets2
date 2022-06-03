05db211a4df455670027dc6197e3bbb061f8b47f
nafissaarnone <95024629+nafissaarnone@users.noreply.github.com>
2022-06-03 17:34:39 +0200

    guide de style

--- src/views/guideVew.vue
+++ src/views/guideVew.vue
@@ -3,15 +3,24 @@
 
         <p class="font-manuale text-gray-900 text">guide de style </p>
 
+
     </div>
       <div name="polisse" class="bg-white m-5 p-3 rounded">
-
+        
         <p class="font-manuale text-gray-900 text">police</p>
+        <span class="font-manuale">manuale</span>
+        <br>
+        <span class="font-codystar">codystar</span>
+        <br>
+        <span class="font-luckiest">luckiest</span>
 
     </div>
       <div name="couleur" class="bg-white m-5 p-3 rounded">
-
+        
         <p class="font-manuale text-gray-900 text">couleur</p>
-
-    </div>
+        <span class="bg-lime-500 ">from-lime-500 </span>
+        <br>
+        <span class="bg-orange-400">to-orange-400</span>
+        <br>
+      </div>
 </template>
